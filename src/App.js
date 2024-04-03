096928061e430c5bab55f432b7fbb104eb7d4c48
pinal24 <155936484+pinal24@users.noreply.github.com>
2024-04-03 21:38:50 +0530

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './Home'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import About from './About'
+import Contact from './Contact'
+import Service from './Service'
+import Footer from './Footer.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+
+import Navbar from './Navbar'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <Routes>
+                <Route exact path='/' element={<Home />}>Home</Route>
+                <Route exact path='/about' element={<About />}>Home</Route>
+                <Route exact path='/service' element={<Service />}>Home</Route>
+                <Route exact path='/contact' element={<Contact />}>Home</Route>
+                <Route path='*' element={<Navigate to='/' />}></Route>
+            </Routes>
+            <Footer/>
+        </div>
+    )
 }
 
-export default App;
+export default App
